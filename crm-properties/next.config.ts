0e72827ec4fc88a3bbda76b48a0eb559b884a85c
Vanja Cvetic <vanja.cvetic@gmail.com>
2026-01-14 23:18:05 +0100

    Podesavanje pocetne konfiguracije Next JS projekta

--- crm-properties/next.config.ts
+++ crm-properties/next.config.ts
@@ -1,7 +1,18 @@
+// next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Preusmeravanje sa "/" na login rutu.
+  // Ovo znaci: kada otvoris http://localhost:3000/ automatski ide na /pages/auth/login
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/pages/auth",
+        permanent: false, // false = privremeno (302), true = trajno (308)
+      },
+    ];
+  },
 };
 
 export default nextConfig;
